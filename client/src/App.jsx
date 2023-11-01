e639c45c126548e092ea9b96c396eb757cde1296
Bhargav Kumar <champion.maverick360@gmail.com>
2023-11-01 17:15:09 +0530

    add pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// Pages
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+
 const App = () => {
   return (
-    <div className="text-center">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/sign-up" element={<SignUp />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/profile" element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
