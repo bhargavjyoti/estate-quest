ca8300210b89af389db9c0f0bcab33c82a807c71
Bhargav Kumar <champion.maverick360@gmail.com>
2023-11-05 19:14:39 +0530

    add loading in button

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -56,7 +56,9 @@ const SignUp = () => {
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
