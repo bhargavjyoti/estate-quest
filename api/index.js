627baeb1756826340a8f28967201d437cd611b7c
Bhargav Kumar <champion.maverick360@gmail.com>
2024-01-17 11:51:36 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
