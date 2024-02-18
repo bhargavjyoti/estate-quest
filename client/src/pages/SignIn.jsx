02c957c7d5992fea97d74e864a3e26178ee15bf7
Bhargav Kumar <champion.maverick360@gmail.com>
2024-02-18 10:36:08 +0530

    fix oauth bug

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import {useDispatch, useSelector} from "react-redux"
 import {signInStart, signInSuccess, signInFailure} from "../redux/user/userSlice" 
-import Oauth from "../components/Oauth"
+import Oauth from "../components/OAuth"
 
 const signin = () => {
   const [formData, setFormData] = useState({})
