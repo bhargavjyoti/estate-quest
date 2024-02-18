eaf991c268988552cdced81b739a292c6f26aa9c
Bhargav Kumar <champion.maverick360@gmail.com>
2024-02-18 10:18:57 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import path from "path"
 
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log(err)
 })
 
+const __dirname = path.resolve()
+
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
@@ -26,6 +29,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
