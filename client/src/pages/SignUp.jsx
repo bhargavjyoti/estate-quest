2c3bec806a15141fd7d60737865c1afa68ba4d28
Bhargav Kumar <champion.maverick360@gmail.com>
2024-02-18 10:39:11 +0530

    fix sign up oauth bug

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import Oauth from "../components/Oauth"
+import Oauth from "../components/OAuth"
 
 const SignUp = () => {
   const [formData, setFormData] = useState({})
